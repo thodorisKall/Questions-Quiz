f1a4d2f480ebccbfc50052b8ff4061fff6d68854
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-10-23 00:46:50 +0300

    docs: initialize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import "./global.css"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
+import Categories from "./Categories"
+import Starter from "./Starter"
+import Question from "./Question"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Starter />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/question' element={<Question />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
