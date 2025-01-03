6f74d503535b8edddac8c6600c71793c4acb8d89
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-01-03 21:57:44 +0200

    feat: add functionality functions and rebuild basic structure

--- src/Question.jsx
+++ src/Question.jsx
@@ -6,6 +6,7 @@ function Question() {
   const location = useLocation()
   const apiUrl = location.state?.apiUrl
   const [questionsUrl, setQuestionsUrl] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     fetch(apiUrl)
@@ -20,31 +21,60 @@ function Question() {
       .catch((err) => console.error(err))
   }, [])
 
+  if (questionsUrl.length === 0) {
+    return <div>Loading...</div>
+  }
+
+  const handleNext = () => {
+    if (currentIndex < questionsUrl.length - 1) {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
+
+  const handlePrevious = () => {
+    if (currentIndex >= 0) {
+      setCurrentIndex(currentIndex - 1)
+    }
+  }
+
+  const handleAnswer = () => {}
+
+  const answers = [
+    ...questionsUrl[currentIndex].incorrect_answers,
+    questionsUrl[currentIndex].correct_answer,
+  ]
+  const shuffleAsnwers = answers.sort(() => Math.random() - 0.5)
+
   return (
     <div>
       <h3>Question</h3>
+
       <div>
-        {questionsUrl.map((data) => {
-          const answers = [...data.incorrect_answers, data.correct_answer]
-          const shuffleAsnwers = answers.sort(() => Math.random() - 0.5)
-          return (
-            <div>
-              <h4>{data.question}</h4>
-              <h5>Answers</h5>
-              <div>
-                {shuffleAsnwers.map((answer) => {
-                  return (
-                    <div>
-                      <button>{answer}</button>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-          )
-        })}
+        <div>
+          <h4>{questionsUrl[currentIndex].question}</h4>
+          <h5>Answers</h5>
+          <div>
+            {shuffleAsnwers.map((answer) => {
+              return (
+                <div key={answer}>
+                  <button>{answer}</button>
+                </div>
+              )
+            })}
+          </div>
+        </div>
       </div>
-      {console.log(questionsUrl)}
+      <div>
+        <button
+          onClick={() => handleNext()}
+          disabled={currentIndex === questionsUrl.length - 1}
+        >
+          Next
+        </button>
+        <button onClick={() => handlePrevious()} disabled={currentIndex === 0}>
+          Previous
+        </button>
+      </div>
     </div>
   )
 }
