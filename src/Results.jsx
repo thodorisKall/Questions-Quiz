eadc78c2b57ce870b7332076ce4abe0d2f0c1d96
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-01-07 00:16:11 +0200

    feat: create func to play win loose or draw sound base on your correct answers

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,9 +1,31 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useLocation } from "react-router-dom"
+import winSoundFile from "./Audio/win.wav"
+import lostSoundFile from "./Audio/loose.mp3"
+import drawSoundFile from "./Audio/draw.wav"
 
 function Results() {
   const location = useLocation()
   const { correctAnswers, wrongAnswers, categoryName } = location.state || {}
+
+  const playSound = () => {
+    const winSound = new Audio(winSoundFile)
+    const lostSound = new Audio(lostSoundFile)
+    const drawSound = new Audio(drawSoundFile)
+
+    if (correctAnswers < wrongAnswers) {
+      lostSound.play()
+    } else if (correctAnswers > wrongAnswers) {
+      winSound.play()
+    } else {
+      drawSound.play()
+    }
+  }
+
+  useEffect(() => {
+    playSound()
+  }, [])
+
   return (
     <div>
       Results of Quiz
