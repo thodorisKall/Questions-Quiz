141f2d7fe5ff9046e9514bfe9bf762f390ed267c
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-01-12 01:38:40 +0200

    feat: add clasnames for css targeting at results component

--- src/Results.jsx
+++ src/Results.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from "react-router-dom"
 import winSoundFile from "./Audio/win.wav"
 import lostSoundFile from "./Audio/loose.mp3"
 import drawSoundFile from "./Audio/draw.wav"
+import { MdOutlineReplay } from "react-icons/md"
 
 function Results() {
   const location = useLocation()
@@ -27,18 +28,32 @@ function Results() {
   }, [])
 
   return (
-    <div>
-      <h2>
+    <div className='results'>
+      <h2
+        className={
+          wrongAnswers > correctAnswers
+            ? "lost"
+            : wrongAnswers < correctAnswers
+            ? "win"
+            : "draw"
+        }
+      >
         {wrongAnswers > correctAnswers
           ? "You Lost"
           : wrongAnswers < correctAnswers
           ? "You Win!!!"
           : "Draw!"}
       </h2>
-      <h3>Category: {categoryName}</h3>
-      <h3>Corrected : {correctAnswers}</h3>
-      <h3>Wrong : {wrongAnswers}</h3>
-      <Link to='/categories'>Play Again</Link>
+      <div className='results__text'>
+        <h4>Category : {categoryName}</h4>
+        <h3>Corrected : {correctAnswers}</h3>
+        <h3>Wrong : {wrongAnswers}</h3>
+      </div>
+
+      <Link to='/categories'>
+        <MdOutlineReplay id='replay--icon' />
+        Play Again
+      </Link>
     </div>
   )
 }
