5d8a6644f90a1a0b80a8c0fc9ef24a0b9e4e1f35
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2025-01-12 23:38:20 +0200

    style: place bars Home button of Nav at left of the screen

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { HiBars3 } from "react-icons/hi2"
 
 function Nav() {
   return (
     <nav>
       <div className='nav--btn'>
         <Link to='/'>Home</Link>
+        <HiBars3 />
       </div>
     </nav>
   )
